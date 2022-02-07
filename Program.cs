355ca6cbc375a492f345ec2f7b57fb4d01e3089e
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-07 23:24:38 +0530

    add multiple AB to the System with a unique Name

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using AddressBook_Using_Collexction;
+using System;
+
+Console.WriteLine("Welcome to Address Book using Collection");
+//uc-6 add multiple Address Book to the System Each Address Book has a unique Name 
+AddressBookCollection.addAddressBook();
+Console.WriteLine("\n");
+AddressBookCollection.ContactsDisplay();
+Console.WriteLine("\n");
+AddressBookCollection.EditContact();
+Console.WriteLine("\n");
+AddressBookCollection.ContactsDisplay();
+Console.WriteLine("\n");
+AddressBookCollection.ContactsDisplay();
+AddressBookCollection.ContactsDisplay();
+AddressBookCollection.ContactsDisplay();
+AddressBookCollection.ContactsDisplay();
