30153578c72cb5b498e981c4c631cf6c63eefd51
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-07 21:56:45 +0530

    adding a person details

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using AddressBook_Using_Collexction;
+using System;
+
+namespace Address_Book_Using_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //uc-1 adding of first and last names, address,city, state, zip, phone number and email...
+            Console.WriteLine("Welcome to Address Book using Collection");
+            AddressBookCollection.AddContact();
+            AddressBookCollection.ContactsDisplay();
+        }
+    }
+}
